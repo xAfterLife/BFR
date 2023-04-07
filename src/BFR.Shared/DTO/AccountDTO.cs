b2786d03306b1376c7de556248550d3b90391ac8
xAfterLife <lisselhard@aol.de>
2023-04-07 13:21:58 +0200

    ä

--- src/BFR.Shared/DTO/AccountDTO.cs
+++ src/BFR.Shared/DTO/AccountDTO.cs
@@ -4,14 +4,8 @@ namespace BFR.Shared.DTO;
 
 public class AccountDto : IDto
 {
+	public long Id { get; }
 	public string Username { get; set; } = null!;
 	public string Password { get; set; } = null!;
 	public byte[] AuthenticationSecret { get; set; } = null!;
-
-	public AccountDto(int id)
-	{
-		Id = id;
-	}
-
-	public long Id { get; }
 }
--- src/BFR.Shared/DTO/AccountDetailsDTO.cs
+++ src/BFR.Shared/DTO/AccountDetailsDTO.cs
@@ -4,17 +4,11 @@ namespace BFR.Shared.DTO;
 
 public class AccountDetailsDto : IDto
 {
+	public long Id { get; }
 	public int AccountId { get; set; }
 	public int Level { get; set; }
 	public int CurrentStamina { get; set; }
 	public int MaxStamina { get; set; }
 	public int Diamonds { get; set; }
 	public long Gold { get; set; }
-
-	public AccountDetailsDto(int id)
-	{
-		Id = id;
-	}
-
-	public long Id { get; }
 }
